a88f11797f3e35105c49ae0b2e8b8f661e60b66e
memoryleak47 <memoryleak47@protonmail.ch>
2026-02-16 20:48:51 +0100

    more basics

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+from dataclasses import dataclass
+
 class Group:
     pass
 
-class Id: pass
+@dataclass(frozen=True)
+class Id:
+    i: int
+
+    def __repr__(self):
+        return "id" + str(self.i)
+
+# slots are integers
+type Slot = int
 
 class Class:
     def __init__(self, arity: int):
         self.group = Group()
         self.arity = arity
         self.leader = None
+
+@dataclass(frozen=True)
+class AppliedId:
+    id: Id
+    args: tuple[Slot]
+
+    def __repr__(self):
+        if self.args:
+            return str(self.id) + "[" + ", ".join(map(str, self.args)) + "]"
+        else:
+            return str(self.id)
 
 class SlottedUF:
     classes: dict[Id, Class]
@@ -16,19 +37,33 @@ class SlottedUF:
         self.classes = {}
 
     def alloc(self, arity: int) -> Id:
-        i = Id()
+        i = Id(len(self.classes))
         self.classes[i] = Class(arity)
         return i
 
+    def find(self, x: AppliedId) -> AppliedId:
+        while True:
+            l = self.classes[x.id].leader
+            if l == None:
+                return x
+            x = l # TODO fix slot order
+
     def union(self, x, y):
-        pass
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+
+        self.classes[x.id].leader = y # TODO fix slot order
 
     def is_equal(self, x, y):
-        pass
-
+        x = self.find(x)
+        y = self.find(y)
+        # TODO handle symmetries
+        return x == y
 
 suf = SlottedUF()
-a = suf.alloc(2)
-b = suf.alloc(2)
+a = AppliedId(suf.alloc(2), (2, 3))
+b = AppliedId(suf.alloc(2), (2, 3))
+print(a)
 suf.union(a, b)
 print(suf.is_equal(a, b))
